a565b4acfa99e9889145bd1b31875c85b42bf171
Juyeong Jang <jjo4342@gmail.com>
2023-12-21 21:23:51 +0900

    Added utils.cpp filse with basic operate

--- src/utils.cpp
+++ src/utils.cpp
@@ -2,3 +2,22 @@
 // Authors: Juyeong Jang
 
 #include "math/utils.hpp"
+
+#include <stdexcept>
+namespace programmers::math {
+    auto Add(double lhs, double rhs)->double {
+        return lhs + rhs;
+    }
+    auto Subtract(double lhs, double rhs)->double {
+        return lhs - rhs;
+    }
+    auto Multiply(double lhs, double rhs)->double {
+        return lhs * rhs;
+    }
+    auto Devide(double lhs, double rhs)->double {
+        return lhs / rhs;
+    }
+    auto IsEqual(double lhs, double rhs)->bool {
+        return (std::abs(lhs-rhs)<std::numeric_limits<double>::epsilon());
+    }
+}
