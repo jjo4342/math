d08c508d8cb3d5a4b3857190c90c2a3637cc1e3a
Juyeong Jang <jjo4342@gmail.com>
2023-12-21 21:20:04 +0900

    Added utils.hpp file

--- include/math/utils.hpp
+++ include/math/utils.hpp
@@ -1,4 +1,14 @@
 // Copyright (c) 2023 Juyeong Jang, All Rights Reserved.
 // Authors: Juyeong Jang
 
+#ifndef PROGRAMMERS__MATH__UTILS_HPP_
+#define PROGRAMMERS__MATH__UTILS_HPP_
+namespace programmers::math {
+    auto Add(double lhs, double rhs)->double;
+    auto Subtract(double lhs, double rhs)->double;
+    auto Multiply(double lhs, double rhs)->double;
+    auto Devide(double lhs, double rhs)->double;
+    auto IsEqual(double lhs, double rhs)->bool;
+}   // namespace programmers::math
 
+#endif  // PROGRAMMERS__MATH__UTILS_HPP_
